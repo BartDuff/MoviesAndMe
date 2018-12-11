92e1db7ac3e56a3091eb18cfe3b8ada5fba2fa51
Florian Bartkowiak <flo@iMac-de-Florian.local>
2018-12-11 02:50:43 +0100

    added infinite scroll down

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,8 +1,8 @@
 const API_TOKEN = "5b5eeec5317ec022a099c24df5f0cd00";
 
-export function getFilmsFromApiWithSearchedText (text) {
+export function getFilmsFromApiWithSearchedText (text, page) {
     const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN +
-    '&language=fr&query=' + text;
+    '&language=fr&query=' + text + '&page=' + page;
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.log(error));
